4defe84664deb369b1c01b98a990ccabf34bd1ca
joaogarin <joao.garin@gmail.com>
2026-02-02 17:58:06 +0000

    sequencial

--- src/image/index.ts
+++ src/image/index.ts
@@ -2,7 +2,14 @@ export { isImageSourceUrl } from "./detector.js";
 export { downloadImageToPng } from "./fetch.js";
 export {
   extractVisualSections,
+  extractSectionsSequentially,
   formatVisualSectionsAsAnalysis,
+  formatSequentialSectionsAsAnalysis,
   takeSectionScreenshotsFromVisualBounds,
 } from "./visual-sections.js";
-export type { VisualSection, VisualSectionsResult } from "./visual-sections.js";
+export type {
+  VisualSection,
+  VisualSectionsResult,
+  DetectedSection,
+  SequentialSectionsResult,
+} from "./visual-sections.js";
